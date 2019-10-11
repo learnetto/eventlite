f9a02122ff87a5df1ccf36c1ecd510a8465e7ce9
Hrishi Mittal <hrishimittal@gmail.com>
2019-10-11 16:45:30 +0100

    Display dummy events data in React component

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,25 @@
 class EventsController < ApplicationController
   def index
+    @events = [
+      {
+        title: "London Retail Expo",
+        datetime: "Monday 14 Oct, 2019 10:00 am GMT",
+        location: "London Excel Centre"
+      },
+      {
+        title: "Enterprise Sales Training Workshop",
+        datetime: "Tuesday 15 Oct, 2019 10:00 am GMT",
+        location: "Expert Sales Company Headquarters"
+      },
+      {
+        title: "Ruby Hack Night",
+        datetime: "Friday 18 Oct, 2019 6:00 pm GMT",
+        location: "Learnetto Headquarters"
+      },
+      {
+        title: "Beginners Salsa dance meetup",
+        datetime: "Saturday 14 Oct, 2019 06:00 pm GMT"
+      }
+    ]
   end
 end
--- app/javascript/packs/eventlite.js
+++ app/javascript/packs/eventlite.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Eventlite = props => (
-  <div>Welcome to Eventlite</div>
+  <div>
+    <h1>Eventlite</h1>
+    {props.events.map(function(e){
+      return(
+        <p>{e.title}</p>
+      )
+    })}
+  </div>
 )
 
 document.addEventListener('DOMContentLoaded', () => {
+  const node = document.getElementById('events_data')
+  const data = JSON.parse(node.getAttribute('data'))
   ReactDOM.render(
-    <Eventlite />,
+    <Eventlite events={data} />,
     document.body.appendChild(document.createElement('div')),
   )
 })
