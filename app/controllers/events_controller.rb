ce4a9d04e5e39aac8ac763ae3f7e8148b0d19ee1
Hrishi Mittal <hrishimittal@gmail.com>
2019-11-01 17:15:54 +0000

    Display date and location for events in list. Add more styling.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,22 +3,22 @@ class EventsController < ApplicationController
     @events = [
       {
         title: "London Retail Expo",
-        datetime: "Monday 14 Oct, 2019 10:00 am GMT",
+        datetime: "Mon, 14 Oct 2019",
         location: "London Excel Centre"
       },
       {
         title: "Enterprise Sales Training Workshop",
-        datetime: "Tuesday 15 Oct, 2019 10:00 am GMT",
+        datetime: "Tue, 15 Oct 2019",
         location: "Expert Sales Company Headquarters"
       },
       {
         title: "Ruby Hack Night",
-        datetime: "Friday 18 Oct, 2019 6:00 pm GMT",
+        datetime: "Fri, 18 Oct 2019",
         location: "Learnetto Headquarters"
       },
       {
         title: "Beginners Salsa dance meetup",
-        datetime: "Saturday 14 Oct, 2019 06:00 pm GMT"
+        datetime: "Sat, 14 Oct 2019"
       }
     ]
   end
--- app/javascript/packs/Event.js
+++ app/javascript/packs/Event.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Event = props => (
-  <div className="event">{props.event.title}</div>
+  <div className="event">
+    <h2 className="event-title">{props.event.title}</h2>
+    <div className="event-datetime">{props.event.datetime}</div>
+    <div className="event-location">{props.event.location}</div>
+  </div>
 )
 
 export default Event
