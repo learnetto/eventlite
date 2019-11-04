08f3110119502d555d6f2a2fc233b12e0bf83c08
Hrishi Mittal <hrishimittal@gmail.com>
2019-11-04 12:36:56 +0000

    Use stored events data in controller. Add formatting function in Event component.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,5 @@
 class EventsController < ApplicationController
   def index
-    @events = [
-      {
-        title: "London Retail Expo",
-        datetime: "Mon, 14 Oct 2019",
-        location: "London Excel Centre"
-      },
-      {
-        title: "Enterprise Sales Training Workshop",
-        datetime: "Tue, 15 Oct 2019",
-        location: "Expert Sales Company Headquarters"
-      },
-      {
-        title: "Ruby Hack Night",
-        datetime: "Fri, 18 Oct 2019",
-        location: "Learnetto Headquarters"
-      },
-      {
-        title: "Beginners Salsa dance meetup",
-        datetime: "Sat, 14 Oct 2019"
-      }
-    ]
+    @events = Event.order('start_datetime ASC')
   end
 end
--- app/javascript/packs/Event.js
+++ app/javascript/packs/Event.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+const formatDate = datetime =>
+  new Date(datetime).toDateString()
+
 const Event = props => (
   <div className="event">
     <h2 className="event-title">{props.event.title}</h2>
-    <div className="event-datetime">{props.event.datetime}</div>
+    <div className="event-datetime">{formatDate(props.event.start_datetime)}</div>
     <div className="event-location">{props.event.location}</div>
   </div>
 )
