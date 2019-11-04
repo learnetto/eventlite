c29b0a75172f91422cea910e3d6f605a9858348d
Hrishi Mittal <hrishimittal@gmail.com>
2019-11-04 15:12:59 +0000

    Convert EventForm to a class Component. Add state and function for handling user input

--- app/javascript/packs/EventForm.js
+++ app/javascript/packs/EventForm.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import Event from './Event'
 
-const EventForm = props => (
-  <div>
-    <h4>Create an Event:</h4>
-    <form>
-      <input type="text" placeholder="Event Title" />
-      <button type="submit">Create Event</button>
-    </form>
-  </div>
-)
+class EventForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      start_datetime: '',
+      location: ''
+    }
+  }
+
+  handleInput = e => {
+    const name = e.target.name;
+    const newState = {};
+    newState[name] = e.target.value;
+    this.setState(newState)
+    e.preventDefault();
+  }
+
+  render () {
+    return (
+      <div>
+        <h4>Create an Event:</h4>
+        <form>
+          <input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleInput} />
+          <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime} onChange={this.handleInput} />
+          <input type="text" name="location" placeholder="Location" value={this.state.location} onChange={this.handleInput} />
+          <button type="submit">Create Event</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default EventForm
