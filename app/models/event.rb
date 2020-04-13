c75774cbfbd91d3738ba4666ef39685048f05409
Hrishi Mittal <hrishimittal@gmail.com>
2020-04-13 13:43:58 +0100

    Add start_datetime validation for past

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   validates_presence_of :title, :start_datetime, :location
   validates :title, length: {minimum: 3}
+  validate :start_datetime_cannot_be_in_the_past
+
+  private
+  def start_datetime_cannot_be_in_the_past
+    if start_datetime.present? && start_datetime < Time.now
+      errors.add(:start_datetime, "can't be in the past")
+    end
+  end
 end
