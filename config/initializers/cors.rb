34ef6011984c0687dbfed31a077c19d96832bd9e
Hrishi Mittal <hrishimittal@gmail.com>
2020-05-29 15:53:30 +0100

    Expose response headers in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '*',
+    headers: :any,
+    expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    methods: [:get, :post, :patch, :put]
   end
 end
