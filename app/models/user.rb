ad545b90c341302f31fa4e1c815447121d3e5c4e
Hrishi Mittal <hrishimittal@gmail.com>
2020-05-29 16:08:58 +0100

    Remove trackable feature from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
